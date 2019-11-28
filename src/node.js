888d4954cb286fb5762753fb51acd828235dc670
alexander <Alexander_yammine@hotmail.com>
2019-11-28 16:36:58 -0400

    Added node file for status and sendraw operations

---
+++ src/node.js
@@ -0,0 +1,23 @@
+const { fetchGet, fetchPost } = require("./http/request");
+const { validateHex } = require("./utils/validates");
+
+async function sendTransaction(config, hexa) {
+	if (!validateHex(hexa)) {
+		throw new Error("Invalid type");
+	}
+	const body = { hexa: hexa };
+	const result = await fetchPost(config.url + "/sendraw", body);
+
+	return result.body;
+}
+
+async function status(config) {
+	const result = await fetchGet(config.url + "/status");
+
+	return result.body;
+}
+
+module.exports = {
+	sendTransaction,
+	status
+};
---
+++ src/utils/validates.js
@@ -0,0 +1,31 @@
+/* eslint-disable require-unicode-regexp */
+
+function validateBase64String(base64) {
+	const regExp = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+
+	return regExp.test(base64);
+}
+
+function validateHex(hex) {
+	const regExp = /^[-+]?[0-9A-Fa-f]+\.?[0-9A-Fa-f]*?$/;
+
+	return regExp.test(hex);
+}
+
+function validateJson(json) {
+	try {
+		JSON.parse(json);
+	}
+	catch (err) {
+		return false;
+	}
+
+	return true;
+}
+
+module.exports = {
+	validateBase64String,
+	validateHex,
+	validateJson
+};
+
