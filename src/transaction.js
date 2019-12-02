4dd234962b06f8b43467cc10f15326d5de5e4d9d
alexander <Alexander_yammine@hotmail.com>
2019-12-02 15:28:36 -0400

    Added more validations to Transactions functions

--- src/transaction.js
+++ src/transaction.js
@@ -8,7 +8,7 @@ async function queryTransactionsCount(config) {
 
 async function queryTransactions(config, id) {
 	if ((typeof (id) !== "string" && typeof (id) !== "number") || id < 0) {
-		throw new Error("Invalid type");
+		throw new Error("Invalid argument, transaction id must be a positive integer or a string hash");
 	}
 	let result;
 	if (typeof (id) === "number") {
@@ -23,7 +23,7 @@ async function queryTransactions(config, id) {
 
 async function queryLatestTransactions(config, count) {
 	if (typeof (count) !== "number" || count < 1 || count > 100) {
-		throw new Error("Invalid type");
+		throw new Error("Invalid argument, COUNT must be a positive integer between 1 and 100");
 	}
 	const result = await fetchGet(config.url + "/transaction/latest/" + count.toString());
 
@@ -31,8 +31,11 @@ async function queryLatestTransactions(config, count) {
 }
 
 async function queryTransactionsFromInterval(config, from, to) {
-	if (typeof (from) !== "number" || typeof (to) !== "number" || (to - from) < 0 || to < 1 || from < 0 || (to - from + 1 > 100)) {
-		throw new Error("Invalid type");
+	if (typeof (from) !== "number" || typeof (to) !== "number" || (to - from) < 0 || from < 0 || to < from) {
+		throw new Error("Invalid arguments, FROM and TO must be a positive integers, and TO must be greater than FROM");
+	}
+	if (to - from + 1 > 100) {
+		throw new Error("Max blocks to query is 100");
 	}
 	const result = await fetchGet(config.url + "/transaction/from/" + from.toString() + "/to/" + to.toString());
 
@@ -40,9 +43,15 @@ async function queryTransactionsFromInterval(config, from, to) {
 }
 
 async function queryTransactionsSince(config, since, until) {
-	if (typeof (since) !== "number" || since < 0 || (until && typeof (until) !== "number") || (until && until < 1) ||
-	(until && (until - since) < 0)) {
-		throw new Error("Invalid type");
+	const date = (new Date().getTime() / 1000) + 1200;
+	if (typeof (since) !== "number" || (until && typeof (until) !== "number")) {
+		throw new Error("Invalid arguments, must be a positive integers");
+	}
+	if (since < 1546300800 || since > date) {
+		throw new Error("Invalid date");
+	}
+	if (until && until < since) {
+		throw new Error("Invalid arguments, UNTIL must be greater than SINCE");
 	}
 	let result;
 	if (until) {
@@ -56,9 +65,15 @@ async function queryTransactionsSince(config, since, until) {
 }
 
 async function queryTransactionsSinceCount(config, since, until) {
-	if (typeof (since) !== "number" || since < 0 || (until && typeof (until) !== "number") || (until && until < 1) ||
-	(until && (until - since) < 0)) {
-		throw new Error("Invalid type");
+	const date = (new Date().getTime() / 1000) + 1200;
+	if (typeof (since) !== "number" || (until && typeof (until) !== "number")) {
+		throw new Error("Invalid arguments, must be a positive integers");
+	}
+	if (since < 1546300800 || since > date) {
+		throw new Error("Invalid date");
+	}
+	if (until && until < since) {
+		throw new Error("Invalid arguments, UNTIL must be greater than SINCE");
 	}
 	let result;
 	if (until) {
