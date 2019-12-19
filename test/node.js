adf3403d0a3fc73d73712aa0923e02f38bc4a66f
alexander <Alexander_yammine@hotmail.com>
2019-12-19 09:41:12 -0400

    removed sendtransaction test

--- test/node.js
+++ test/node.js
@@ -17,34 +17,4 @@ describe("Node operations", function() {
 		});
 	});
 
-	it("It should send transaction", function(done) {
-		client.queryBlocksCount()
-		.then((blocksCount) => {
-			const mnemonic = "obtain extend cheap want ride fatal jungle reject field sell arm apology" +
-			" avocado grit ball enough rebuild false celery favorite cook soon talk abandon hope";
-			const keys = algosdk.mnemonicToSecretKey(mnemonic);
-			const txn = {
-				"from": keys.addr,
-				"to": "46QNIYQEMLKNOBTQC56UEBBHFNH37EWLHGT2KGL3ZGB4SW77W6V7GBKPDY",
-				"fee": 1000,
-				"amount": 10000,
-				"firstRound": blocksCount + 1,
-				"lastRound": blocksCount + 100,
-				"genesisID": client.getGenesisId(),
-				"genesisHash": client.getGenesisHash()
-			};
-			const signedTxn = algosdk.signTransaction(txn, keys.sk);
-			const hexa = Buffer.from(signedTxn.blob).toString('hex');
-			client.sendTransaction(hexa)
-			.then(() => {
-				done();
-			})
-			.catch((err) => {
-				done(err);
-			});
-		})
-		.catch((err) => {
-			done(err);
-		});
-	});
 });
