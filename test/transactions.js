e4d7084d4a36bfea75ca588e0f3ba69a0345c7bf
JCibeira <jacibcas@gmail.com>
2019-12-20 21:35:54 -0400

    Transaction operations

--- test/transactions.js
+++ test/transactions.js
@@ -2,7 +2,7 @@ const AlgoexplorerApi = require("../index");
 const expect = require("chai").expect;
 const client = new AlgoexplorerApi("testnet");
 
-describe("Account operations", function() {
+describe("Transaction operations", function() {
 	// eslint-disable-next-line no-invalid-this
 	this.timeout(5000);
 	it("It should query transaction by a specified index", function(done) {
